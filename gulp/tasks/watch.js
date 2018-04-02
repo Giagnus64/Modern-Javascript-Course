39f8822bf385157c8d31d28c4a7403a89c10b8e4
Giagnus64 <28762593+Giagnus64@users.noreply.github.com>
2018-04-02 17:08:57 -0400

    Added gulp functionality and proper fws.

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+//defining gulp watch tasks
+gulp.task('watch', function(){
+	//initialize browsersync server
+	browserSync.init({
+		//makes browsersync notifications dissappear
+		//notify: false,
+		server:{
+			baseDir:"task_list"
+		}
+	});
+
+	//reload browser on changes
+	watch('./task_list/index.html', function(){
+		browserSync.reload();
+	});
+	
+	watch('./task_list/assets/styles/**/*.css', function(){
+		gulp.start('cssInject');
+		//browserSync.reload();
+	});
+
+	watch('./task_list/assets/scripts/**/*.js', function(){
+		//gulp.start('scriptsRefresh');
+		browserSync.reload();
+	});
+});
+
+//new task for browsersync inject css - with styles task dependency
+gulp.task('cssInject',['styles'], function(){
+	gulp.src('./task_list/temp/styles/styles.css')
+	.pipe(browserSync.stream());
+});
+
+/*//new task for browsersync to refresh after webpack repacks JS
+gulp.task('scriptsRefresh', ['scripts'], function(){
+	browserSync.reload();
+})*/

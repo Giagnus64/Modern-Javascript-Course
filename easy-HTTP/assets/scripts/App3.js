d37282996473c4266b2dfa56808a983db231afa3
Giagnus64 <28762593+Giagnus64@users.noreply.github.com>
2018-04-17 15:40:40 -0400

    finished Easy-HTTP library part 3

---
+++ easy-HTTP/assets/scripts/App3.js
@@ -0,0 +1,31 @@
+const http = new EasyHTTP;
+
+//Get Users
+// http.get('https://jsonplaceholder.typicode.com/users/')
+// 	.then(data => console.log(data))
+// 	.catch(err => console.log(err));
+
+//Create data
+const data = {
+	name:'John Doe',
+	username: 'johndoe',
+	email:'you@u.com'
+}
+
+
+//POST User
+// http.post('https://jsonplaceholder.typicode.com/users/', data)
+// 	.then(data => console.log(data))
+// 	.catch(err => console.log(err));
+
+
+//PUT User
+http.put('https://jsonplaceholder.typicode.com/users/1', data)
+	.then(data => console.log(data))
+	.catch(err => console.log(err));
+
+//DELETE User
+http.remove('https://jsonplaceholder.typicode.com/users/1')
+	.then(data => console.log(data))
+	.catch(err => console.log(err));
+

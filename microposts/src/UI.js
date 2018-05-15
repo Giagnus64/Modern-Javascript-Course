c8c845fffd26ae687c967c9900440cb62d49fc3c
Giagnus64 <28762593+Giagnus64@users.noreply.github.com>
2018-05-15 03:45:27 -0400

    Finished Microposts project.

---
+++ microposts/src/UI.js
@@ -0,0 +1,123 @@
+class UI{
+	constructor(){
+		this.post = document.querySelector('.posts');
+		this.titleInput = document.querySelector('#title');
+		this.titleInput = document.querySelector('#title');
+		this.bodyInput = document.querySelector('#body');
+		this.idInput = document.querySelector('#id');
+		this.postSubmit = document.querySelector('.post-submit');
+		this.formState = 'add';
+	}
+	//Show All Posts
+	showPosts(posts){
+		let output = '';
+
+		posts.forEach((post) =>{
+			output += `
+			<div class="card mb-3">
+				<div class="card-body">
+					<h4 class="card-title">${post.title}</h4>
+					<p class="card-text">${post.body}</p>
+					<a href="#" class="edit card-link" data-id="${post.id}">
+						<i class="fa fa-pencil"></i>
+					</a>
+					<a href="#" class="delete card-link" data-id="${post.id}">
+						<i class="fa fa-remove"></i>
+					</a>
+				</div>
+			</div>
+			`;
+		});
+
+		this.post.innerHTML = output;
+	}
+	//Show alert message
+	showAlert(message, className){
+		this.clearAlert();
+
+		//Create div
+		const div = document.createElement('div');
+		//Add classes
+		div.className = className;
+		//Add text
+		div.appendChild(document.createTextNode(message));
+		//Get Parent
+		const container = document.querySelector('.postsContainer');
+		//Get posts
+		const posts = document.querySelector('.posts');
+		//Insert alert div before posts in container before posts div
+		container.insertBefore(div, posts);
+
+		//Timeout
+		setTimeout(() => {
+			this.clearAlert();
+		}, 3000);
+
+	}
+	//Clear Alert
+	clearAlert(){
+		const currentAlert = document.querySelector('.alert');
+
+		if(currentAlert){
+			currentAlert.remove();
+		}
+
+	}
+	// clear form fields
+	clearFields(){
+		this.titleInput.value = '';
+		this.bodyInput.value = '';
+	}
+
+	//Fill form to edit
+	fillForm(data){
+		this.titleInput.value = data.title;
+		this.bodyInput.value = data.body;
+		this.idInput.value = data.id;
+
+		this.changeFormState('edit');
+
+	}
+
+	//Clear ID hidden field
+	clearIdInput(){
+		this.idInput.value = '';
+	}
+	//Change state to edit
+	changeFormState(type){
+		if(type === 'edit'){
+			if(document.querySelector('.post-cancel') === null){
+			this.postSubmit.textContent = 'Update Post';
+			this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+			//Create cancel button
+			const button = document.createElement('button');
+			button.className = 'post-cancel btn btn-light btn-block';
+			button.appendChild(document.createTextNode('Cancel Edit'));
+
+			//Get Parent
+			const cardForm = document.querySelector('.card-form');
+			//Get element to insert before
+			const formEnd = document.querySelector('.form-end');
+			//Insert button
+			cardForm.insertBefore(button, formEnd); 
+		}
+
+		} else{
+			this.postSubmit.textContent = 'Post it!';
+			this.postSubmit.className = "post-submit btn btn-primary btn-block";
+
+			//Remove cancel button if there
+			if(document.querySelector('.post-cancel')){
+				document.querySelector('.post-cancel').remove();
+			}
+			//Clear ID from hidden field
+			this.clearIdInput();
+			this.clearFields();
+
+		}
+	}
+
+}
+
+export const ui = new UI();
